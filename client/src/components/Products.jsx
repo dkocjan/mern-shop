58dae72d6a0c3ba18bd4ea2015739d3f540a633f
dkocjan <dkocjan123@gmail.com>
2018-03-23 15:56:18 +0100

    Add Products component

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,96 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Card,
+  Input,
+  Divider,
+  Dimmer,
+  Segment,
+} from 'semantic-ui-react';
+import CardPlaceholder from './CardPlaceholder';
+
+import ProductCard from './ProductCard';
+
+class Home extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.props.onUserInput(e.target.value);
+    e.preventDefault();
+  }
+
+  render() {
+    const { products } = this.props;
+
+    const filteredProducts = this.props.products.filter(
+      product =>
+        product.name
+          .concat(product.description)
+          .toLowerCase()
+          .indexOf(this.props.filterText.toLowerCase()) !== -1
+    );
+
+    return (
+      <Container fluid>
+        <Input
+          fluid
+          icon="search"
+          placeholder="Search products..."
+          focus
+          onChange={this.handleInputChange}
+        />
+        <Divider />
+        {products.length !== 0 ? (
+          <Card.Group>
+            {filteredProducts.map(product => (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                images={product.images}
+                category={product.category}
+                tags={product.tags}
+                id={product.id}
+                currency={this.props.currency}
+              />
+            ))}
+          </Card.Group>
+        ) : (
+          <Dimmer.Dimmable
+            as={Segment}
+            style={{ height: 1000 }}
+            blurring
+            dimmed
+          >
+            <Dimmer active>
+              <Card.Group>
+                <CardPlaceholder />
+                <CardPlaceholder />
+                <CardPlaceholder />
+                <CardPlaceholder />
+                <CardPlaceholder />
+                <CardPlaceholder />
+              </Card.Group>
+            </Dimmer>
+          </Dimmer.Dimmable>
+        )}
+      </Container>
+    );
+  }
+}
+
+Home.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currency: PropTypes.objectOf(PropTypes.string).isRequired,
+  onUserInput: PropTypes.func.isRequired,
+  filterText: PropTypes.string.isRequired,
+};
+
+export default Home;
